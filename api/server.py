87ec4aaeb72aee87d5bdc60fb145f3a947482dd4
Dana Fallon <danajfallon@gmail.com>
2018-12-01 16:29:07 -0800

    Connect server to firebase; add basic endpoints for signing up & logging in

--- api/server.py
+++ api/server.py
@@ -1,14 +1,50 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import pyrebase
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify(data='hello')
+firebase_config = {
+    "apiKey": os.environ.get('FIREBASE_API_KEY'),
+    "authDomain": "reunitefamilies-28bee.firebaseapp.com",
+    "databaseURL": "https://reunitefamilies-28bee.firebaseio.com",
+    "storageBucket": "reunitefamilies-28bee.appspot.com"
+}
+firebase = pyrebase.initialize_app(firebase_config)
+
+
+@app.route('/register')
+def register():
+    data = request.form
+    email = data['email']
+    password = data['password']
+    user = firebase.auth().create_user_with_email_and_password(email, password)
+    volunteer = {
+        'firebase_id': user['uid'],
+        'name': data['name'],
+        'location': data['location'],
+        'languages': data['languages']
+    }
+    firebase.child('volunteers').push(volunteer)
+    return 'registered'
+
+
+@app.route('/login')
+def login():
+    data = request.form
+    email = data['email']
+    password = data['password']
+    firebase.auth().sign_in_with_email_and_password(email, password)
+    return 'logged in'
+
+
+@app.route('/current-user')
+def current_user():
+    user = firebase.auth().current_user
+    return jsonify(current_user=user)
 
 
 if __name__ == "__main__":
